45d079e10911019570d0ef872fecd28f2d08179f
vish jr <135554020+juniorvish@users.noreply.github.com>
2023-06-05 21:33:45 +0530

    Update GoMagentoAPIs/main.go

--- GoMagentoAPIs/main.go
+++ GoMagentoAPIs/main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"GoMagentoAPIs/auth"
-	"GoMagentoAPIs/api"
+	"GoMagentoAPIs/apis"
 	"log"
 	"net/http"
 )
 
 func main() {
-	authMiddleware := auth.AuthMiddleware()
+	authToken, err := auth.GetAuthToken()
+	if err != nil {
+		log.Fatalf("Error getting auth token: %v", err)
+	}
 
-	http.HandleFunc("/api/products", authMiddleware(api.GetAllProducts))
-	http.HandleFunc("/api/customers", authMiddleware(api.GetAllCustomers))
-	http.HandleFunc("/api/orders", authMiddleware(api.GetAllOrders))
-	http.HandleFunc("/api/payments", authMiddleware(api.GetAllPayments))
-	http.HandleFunc("/api/product/", authMiddleware(api.GetProductByID))
-	http.HandleFunc("/api/customer/", authMiddleware(api.GetCustomerByID))
-	http.HandleFunc("/api/order/", authMiddleware(api.GetOrderByID))
+	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetAllProducts(w, r, authToken)
+	})
+	http.HandleFunc("/customers", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetAllCustomers(w, r, authToken)
+	})
+	http.HandleFunc("/orders", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetAllOrders(w, r, authToken)
+	})
+	http.HandleFunc("/payments", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetAllPayments(w, r, authToken)
+	})
+	http.HandleFunc("/product", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetProductById(w, r, authToken)
+	})
+	http.HandleFunc("/customer", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetCustomerById(w, r, authToken)
+	})
+	http.HandleFunc("/order", func(w http.ResponseWriter, r *http.Request) {
+		apis.GetOrderById(w, r, authToken)
+	})
 
-	log.Println("Server running on port 8080")
+	log.Println("Server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
